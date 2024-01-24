3ac8b11b69de91c1f4d4f223086947a2a98efe65
Kevinwoolworth <87849802+Kevinwoolworth@users.noreply.github.com>
2024-01-25 09:16:43 +1100

    update

---
+++ first_script.py
@@ -0,0 +1,77 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver import ActionChains
+
+
+PATH = "chromedriver"
+
+# driver = webdriver.Chrome()
+
+# driver.get("https://www.selenium.dev/selenium/web/web-form.html")
+
+# title = driver.title
+# driver.implicitly_wait(0.5)
+
+# text_box = driver.find_element(by=By.NAME, value="my-text")
+# submit_button = driver.find_element(by=By.CSS_SELECTOR, value="button")
+
+# text_box.send_keys("Selenium")
+# submit_button.click()
+
+# message = driver.find_element(by=By.ID, value="message")
+# text = message.text
+
+# time.sleep(3)
+# driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+# time.sleep(3)
+# SCROLL_PAUSE_TIME = 0.5
+
+# # Get scroll height
+# last_height = driver.execute_script("return document.body.scrollHeight")
+
+# while True:
+#     # Scroll down to bottom
+#     driver.get("https://medium.com/@ozbillwang/vercel-two-use-cases-i-learned-recently-e0e07b21ede5")  
+#     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+#     driver.maximize_window()
+#     # Wait to load page
+#     # time.sleep(SCROLL_PAUSE_TIME)
+
+#     # Calculate new scroll height and compare with last scroll height
+#     new_height = driver.execute_script("return document.body.scrollHeight")
+#     if new_height == last_height:
+#         break
+#     last_height = new_height
+
+# driver.quit()
+
+# url_to_open='https://medium.com/@ozbillwang/vercel-two-use-cases-i-learned-recently-e0e07b21ede5'
+url_to_open='https://medium.com/p/3f4d13258591'
+
+
+
+num_iterations = 50
+
+# Loop to open URL, perform actions, and close browser
+for i in range(num_iterations):
+    # Create a new instance of the WebDriver (e.g., Chrome)
+    driver = webdriver.Chrome()  # You can use other browser drivers as well
+
+    # Open the URL
+    driver.get(url_to_open)
+
+    # Perform some actions (you can add your specific actions here)
+    # For example, printing the page title
+    print("Iteration:", i + 1, "- Page Title:", driver.title)
+    driver.maximize_window()
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    # Wait for a moment (you can adjust this as needed)
+    time.sleep(2)
+
+    # Close the browser
+    driver.quit()
+
+    # Wait for a moment before starting the next iteration (optional)
+    time.sleep(2)
